4e430300b4fe4e8db53fb239a3da9e4e87ba09aa
daksh <daksh@indiamart.com>
2021-04-20 23:40:22 +0530

    fix the issue

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,19 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import dunkin from '../../assets/dunkin_donut.png';
 
 const Home = () => {
     return (
         <React.Fragment>
         
-		<div className="container">
-                	<h1>Welcome</h1>
+		<div className="container" style={{backgroundImage:}}>  
+                	<h1>Welcome! Please search the Recipe or select the Search Menu</h1>
 			<div className="ui three column grid">
 				<div className="column">
 					<div className="ui fluid card">
 						<div className="image">
-							<img alt="daniel" src="../../../assets/rcpie.jpeg"/>
+							<img alt="daniel" src={dunkin}/>
 						</div>
 						<div className="content">
 							<NavLink className="header" to="/SearchRecipe">Search Recipe</NavLink>
