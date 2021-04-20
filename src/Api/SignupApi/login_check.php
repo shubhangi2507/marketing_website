7155b06a0dd7f397b382cc75379a9568bfef879f
vishalpunj <44432313+vishalpunj@users.noreply.github.com>
2021-04-20 18:51:01 +0530

    add

--- src/Api/SignupApi/login_check.php
+++ src/Api/SignupApi/login_check.php
@@ -5,13 +5,18 @@ if ( $_REQUEST['email'] != "" && $_REQUEST["password"]) {
     $str = file_get_contents('http://localhost/users.json');
     $json = json_decode($str, true);
     // print_r($json);
-    $insert_new_user = true;
+    $user_found = true;
     foreach ($json as $item) {
         // echo "json email:".$json["email"]." request email:".$_REQUEST['email']."\n";
         if ($item["email"] == $_REQUEST['email'] && $item["password"] == $_REQUEST['password']) {
             echo "Hi there welcome to the login";
             $insert_new_user = false;
-        } 
+        } else {
+            $user_found = false;
+        }
+    }
+    if(!$user_found) {
+        echo "user does not exist";
     }
     // if ($insert_new_user) {
     //     $data = [
